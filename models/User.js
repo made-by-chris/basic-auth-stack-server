03e25ff4d6a60e291c3f66814b286c443020861c
z-memory <dont@forgetmy.email>
2022-03-19 12:03:44 +0100

    add user schema password hashing and comparison method

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const SALT_WORK_FACTOR = 10;
 
 // a schema is a description of what data should look like
 const UserSchema = new mongoose.Schema(
@@ -9,6 +11,46 @@ const UserSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+UserSchema.pre("save", function (next) {
+  var user = this;
+  if (!user.isModified("password")) return next();
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+UserSchema.statics.authenticate = function (email, password, callback) {
+  User.findOne({ email: email }).exec(function (err, user) {
+    if (err) {
+      return callback(err);
+    } else if (!user) {
+      var err = new Error("User not found.");
+      err.status = 401;
+      return callback(err);
+    }
+    bcrypt.compare(password, user.password, function (err, result) {
+      if (err) {
+        return callback(err);
+      }
+      if (result === true) {
+        return callback(null, user);
+      } else {
+        var err = new Error("Incorrect password.");
+        err.status = 402;
+        return callback(err);
+      }
+    });
+  });
+};
 
 // model is the tool you use to interact with DB
 const User = mongoose.model("User", UserSchema);
